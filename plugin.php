542c22bf690ef4730fbd74c77f0408f575396079
Mike Hansen <web.mhansen@gmail.com>
2014-06-11 14:19:10 -0600

    Prepare plugin for new banner urls

--- plugin.php
+++ plugin.php
@@ -2,11 +2,13 @@
 /*
 Plugin Name: Bluehost Affiliator
 Description: This plugin makes it easy for you to add Bluehost affiliate banners to posts using a Bluehost icon above the editor.  You can also add static banners to the sidebar with the widget.  To get started insert your Bluehost Affiliate Username under Settings -> General.
-Version: 1.0.3
+Version: 1.0.4
 Author: Mike Hansen
-Author URI: http://mikehansen.me
+Author URI: http://mikehansen.me?utm_source=bha_wp_plugin
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
+GitHub Plugin URI: bluehost/wp-affiliator
+GitHub Branch: master
 */
 
 function bha_plugin_action_links( $links, $file ) {
@@ -33,7 +35,7 @@ function bha_add_inline_popup_content() {
 ?>
 <div id="bha_popup_container" style="display:none;">
 	<form id="bha-form">
-	<div class="bha-preview"><img src="http://img.bluehost.com/100x100/bh_100x100_01.gif" /></div>
+	<div class="bha-preview"><img src="<?php echo bha_get_image_link(); ?>" /></div>
 	<label for="bha-img-size">Size : </label>
 	<select name="bha-img-size" class="bha-img-size">
 		<?php
@@ -79,6 +81,13 @@ function bha_img_js() {
 <script type="text/javascript">
 jQuery( document ).ready( function() {
 	var bha_links = <?php echo json_encode( bha_image_sizes() ); ?>;
+	function bha_image_links( size, variant ) {
+		if( typeof bha_links[ size ][ variant ] == 'undefined' ) {
+			return bha_links[ size ]['03'];
+		} else {
+			return bha_links[ size ][ variant ];
+		}
+	}
 	function bha_build_shortcode() {
 		var size = jQuery( '.bha-img-size' ).val();
 		var variation = jQuery( '.bha-img-variation' ).val();
@@ -96,7 +105,7 @@ jQuery( document ).ready( function() {
 	jQuery( '.bha-img-size, .bha-img-variation' ).change( function() {
 		var size = jQuery( '.bha-img-size' ).val();
 		var variation = jQuery( '.bha-img-variation' ).val();
-		jQuery( '.bha-preview' ).html( '<img src="http://img.bluehost.com' + bha_links[size][variation] + '" />' );
+		jQuery( '.bha-preview' ).html( '<img src="https://bluehost-cdn.com/media/partner/images/house' + bha_image_links( size, variation ) + '" />' );
 	} );
 	//this is for the widget
 	jQuery( document ).on( 'change', '.bha-wid-img-size, .bha-wid-img-variation', function() {	
@@ -107,7 +116,7 @@ jQuery( document ).ready( function() {
 		} else {
 			var message = "<small>(preview)</small>";
 		}
-		jQuery( this ).parent().siblings( '.bha-wid-preview' ).html( '<img src="http://img.bluehost.com' + bha_links[size][variation] + '" /><br />' + message );
+		jQuery( this ).parent().siblings( '.bha-wid-preview' ).html( '<img src="https://bluehost-cdn.com/media/partner/images/house' + bha_image_links( size, variation ) + '" /><br />' + message );
 	} );
 	jQuery( '#bha-form' ).submit( function( e ) {       
 		e.preventDefault();
@@ -130,8 +139,8 @@ add_action( 'admin_footer-index.php', 'bha_img_js' );
 
 function bha_shortcode( $atts ) {
 	$defaults = array(
-		'size'		=> '100x100',
-		'variation' => '1',
+		'size'		=> '120x120',
+		'variation' => '3',
 		'align'		=> 'none',
 		'position'	=> 'before',
 		'id'		=> ''
@@ -144,7 +153,7 @@ function bha_shortcode( $atts ) {
 	}
 	$aff_link = bha_get_link( $id  );
 	$img_srcs = bha_image_sizes();
-	return "<a href='" . $aff_link . "' target='_blank'><img class='" . $atts['align'] . "' src='http://img.bluehost.com/" . $img_srcs[ $atts['size'] ][ $atts['variation'] ] . "' /></a>";
+	return "<a href='" . $aff_link . "' target='_blank'><img class='" . $atts['align'] . "' src='" . bha_get_image_link( $atts['size'], $atts['variation'] ) . "' /></a>";
 }
 add_shortcode( 'bha', 'bha_shortcode' );
 
@@ -229,191 +238,51 @@ add_action( 'admin_head-page.php', 'bha_admin_styles' );
 add_action( 'admin_head-widgets.php', 'bha_admin_styles' );
 add_action( 'admin_head-index.php', 'bha_admin_styles' );
 
+function bha_image_base() {
+	$base = "http://bluehost-cdn.com/media/partner/images/";
+	$base .= get_option( 'bha_username', bha_guess_username() ) . "/";
+	return $base;
+}
+
+function bha_get_image_link( $size = "120x120", $variant = '03' ) {
+	$base = bha_image_base();
+	$url = "https://bluehost-cdn.com/media/partner/images/";
+	$user = get_option( 'bha_username', bha_guess_username() );
+	$images = bha_image_sizes();
+	if( isset( $images[ $size ][ $variant ] ) ) {
+		$image = $images[ $size ][ $variant ];
+	} elseif( isset( $images[ $size ] ) ) {
+		$image = $images[ $size ]['03'];
+	} else {
+		$image = $images['120x120']['03'];
+	}
+	return $url . $user . $image;
+}
+
 function bha_image_sizes() {
-	$images = array(
-		'100x100' => array(
-			'01' => '/100x100/bh_100x100_01.gif',
-			'02' => '/100x100/bh_100x100_02.gif',
-			'03' => '/100x100/bh_100x100_03.gif',
-			'04' => '/100x100/bh_100x100_04.gif',
-			'05' => '/100x100/bh_100x100_05.gif'
-			),
-		'120x120' => array(
-			'01' => '/120x120/bh_120x120_01.gif',
-			'02' => '/120x120/bh_120x120_02.gif',
-			'03' => '/120x120/bh_120x120_03.gif',
-			'04' => '/120x120/bh_120x120_04.gif',
-			'05' => '/120x120/bh_120x120_05.gif'
-			),
-		'120x240' => array(
-			'01' => '/120x240/bh_120x240_01.gif',
-			'02' => '/120x240/bh_120x240_02.gif',
-			'03' => '/120x240/bh_120x240_03.gif',
-			'04' => '/120x240/bh_120x240_04.gif',
-			'05' => '/120x240/bh_120x240_05.gif'
-			),
-		'120x60' => array(
-			'01' => '/120x60/bh_120x60_01.gif',
-			'02' => '/120x60/bh_120x60_02.gif',
-			'03' => '/120x60/bh_120x60_03.gif',
-			'04' => '/120x60/bh_120x60_04.gif',
-			'05' => '/120x60/bh_120x60_05.gif'
-			),
-		'120x600' => array(
-			'01' => '/120x600/bh_120x600_01.gif',
-			'02' => '/120x600/bh_120x600_02.gif',
-			'03' => '/120x600/bh_120x600_03.gif',
-			'04' => '/120x600/bh_120x600_04.gif',
-			'05' => '/120x600/bh_120x600_05.gif'
-			),
-		'120x90' => array(
-			'01' => '/120x90/bh_120x90_01.gif',
-			'02' => '/120x90/bh_120x90_02.gif',
-			'03' => '/120x90/bh_120x90_03.gif',
-			'04' => '/120x90/bh_120x90_04.gif',
-			'05' => '/120x90/bh_120x90_05.gif'
-			),
-		'125x125' => array(
-			'01' => '/125x125/bh_125x125_01.gif',
-			'02' => '/125x125/bh_125x125_02.gif',
-			'03' => '/125x125/bh_125x125_03.gif',
-			'04' => '/125x125/bh_125x125_04.gif',
-			'05' => '/125x125/bh_125x125_05.gif'
-			),
-		'125x240' => array(
-			'01' => '/125x240/bh_125x240_01.gif',
-			'02' => '/125x240/bh_125x240_02.gif',
-			'03' => '/125x240/bh_125x240_03.gif',
-			'04' => '/125x240/bh_125x240_04.gif',
-			'05' => '/125x240/bh_125x240_05.gif'
-			),
-		'125x400' => array(
-			'01' => '/125x400/bh_125x400_01.gif',
-			'02' => '/125x400/bh_125x400_02.gif',
-			'03' => '/125x400/bh_125x400_03.gif',
-			'04' => '/125x400/bh_125x400_04.gif',
-			'05' => '/125x400/bh_125x400_05.gif'
-			),
-		'160x40' => array(
-			'01' => '/160x40/bh_160x40_01.gif',
-			'02' => '/160x40/bh_160x40_02.gif',
-			'03' => '/160x40/bh_160x40_03.gif',
-			'04' => '/160x40/bh_160x40_04.gif',
-			'05' => '/160x40/bh_160x40_05.gif'
-			),
-		'160x600' => array(
-			'01' => '/160x600/bh_160x600_01.gif',
-			'02' => '/160x600/bh_160x600_02.gif',
-			'03' => '/160x600/bh_160x600_03.gif',
-			'04' => '/160x600/bh_160x600_04.gif',
-			'05' => '/160x600/bh_160x600_05.gif'
-			),
-		'175x25' => array(
-			'01' => '/175x25/bh_175x25_01.gif',
-			'02' => '/175x25/bh_175x25_02.gif',
-			'03' => '/175x25/bh_175x25_03.gif',
-			'04' => '/175x25/bh_175x25_04.gif',
-			'05' => '/175x25/bh_175x25_05.gif'
-			),
-		'175x30' => array(
-			'01' => '/175x30/bh_175x30_01.gif',
-			'02' => '/175x30/bh_175x30_02.gif',
-			'03' => '/175x30/bh_175x30_03.gif',
-			'04' => '/175x30/bh_175x30_04.gif',
-			'05' => '/175x30/bh_175x30_05.gif'
-			),
-		'180x150' => array(
-			'01' => '/180x150/bh_180x150_01.gif',
-			'02' => '/180x150/bh_180x150_02.gif',
-			'03' => '/180x150/bh_180x150_03.gif',
-			'04' => '/180x150/bh_180x150_04.gif',
-			'05' => '/180x150/bh_180x150_05.gif'
-			),
-		'189x116' => array(
-			'01' => '/189x116/bh_189x116_01.gif',
-			'02' => '/189x116/bh_189x116_02.gif',
-			'03' => '/189x116/bh_189x116_03.gif',
-			'04' => '/189x116/bh_189x116_04.gif',
-			'05' => '/189x116/bh_189x116_05.gif'
-			),
-		'190x60' => array(
-			'01' => '/190x60/bh_190x60_01.gif',
-			'02' => '/190x60/bh_190x60_02.gif',
-			'03' => '/190x60/bh_190x60_03.gif',
-			'04' => '/190x60/bh_190x60_04.gif',
-			'05' => '/190x60/bh_190x60_05.gif'
-			),
-		'234x60' => array(
-			'01' => '/234x60/bh_234x60_01.gif',
-			'02' => '/234x60/bh_234x60_02.gif',
-			'03' => '/234x60/bh_234x60_03.gif',
-			'04' => '/234x60/bh_234x60_04.gif',
-			'05' => '/234x60/bh_234x60_05.gif'
-			),
-		'300x250' => array(
-			'01' => '/300x250/bh_300x250_01.gif',
-			'02' => '/300x250/bh_300x250_02.gif',
-			'03' => '/300x250/bh_300x250_03.gif',
-			'04' => '/300x250/bh_300x250_04.jpg',
-			'05' => '/300x250/bh_300x250_05.gif'
-			),
-		'300x50' => array(
-			'01' => '/300x50/bh_300x50_01.gif',
-			'02' => '/300x50/bh_300x50_02.gif',
-			'03' => '/300x50/bh_300x50_03.gif',
-			'04' => '/300x50/bh_300x50_04.gif',
-			'05' => '/300x50/bh_300x50_05.gif'
-			),
-		'430x288' => array(
-			'01' => '/430x288/bh_430x288_01.gif',
-			'02' => '/430x288/bh_430x288_02.gif',
-			'03' => '/430x288/bh_430x288_03.gif',
-			'04' => '/430x288/bh_430x288_04.jpg',
-			'05' => '/430x288/bh_430x288_05.gif'
-			),
-		'468x60' => array(
-			'01' => '/468x60/bh_468x60_01.gif',
-			'02' => '/468x60/bh_468x60_02.gif',
-			'03' => '/468x60/bh_468x60_03.gif',
-			'04' => '/468x60/bh_468x60_04.gif',
-			'05' => '/468x60/bh_468x60_05.gif'
-			),
-		'488x160' => array(
-			'01' => '/488x160/bh_488x160_01.gif',
-			'02' => '/488x160/bh_488x160_02.gif',
-			'03' => '/488x160/bh_488x160_03.gif',
-			'04' => '/488x160/bh_488x160_04.gif',
-			'05' => '/488x160/bh_488x160_05.gif'
-			),
-		'620x203' => array(
-			'01' => '/620x203/bh_620x203_01.jpg',
-			'02' => '/620x203/bh_620x203_02.jpg',
-			'03' => '/620x203/bh_620x203_03.jpg',
-			'04' => '/620x203/bh_620x203_04.jpg',
-			'05' => '/620x203/bh_620x203_05.jpg'
-			),
-		'728x90' => array(
-			'01' => '/728x90/bh_728x90_01.gif',
-			'02' => '/728x90/bh_728x90_02.gif',
-			'03' => '/728x90/bh_728x90_03.gif',
-			'04' => '/728x90/bh_728x90_04.gif',
-			'05' => '/728x90/bh_728x90_05.gif'
-			),
-		'760x80' => array(
-			'01' => '/760x80/bh_760x80_01.gif',
-			'02' => '/760x80/bh_760x80_02.gif',
-			'03' => '/760x80/bh_760x80_03.gif',
-			'04' => '/760x80/bh_760x80_04.gif',
-			'05' => '/760x80/bh_760x80_05.gif'
-			),
-		'88x31' => array(
-			'01' => '/88x31/bh_88x31_01.gif',
-			'02' => '/88x31/bh_88x31_02.gif',
-			'03' => '/88x31/bh_88x31_03.gif',
-			'04' => '/88x31/bh_88x31_04.gif',
-			'05' => '/88x31/bh_88x31_05.gif'
-			)
-		);
+	$sizes = array(
+		'120x120' 	=> array( 3, 4 ),
+		'120x240' 	=> array( 3, 4 ),
+		'120x600' 	=> array( 2, 3 ),
+		'125x125' 	=> array( 3, 4 ),
+		'125x240' 	=> array( 3, 4 ),
+		'125x400' 	=> array( 3, 4 ),
+		'160x40' 	=> array( 3, 4 ),
+		'160x600' 	=> array( 1, 3),
+		'190x60' 	=> array( 3, 4 ),
+		'300x250' 	=> array( 1, 2, 3, 4 ),
+		'430x288' 	=> array( 1, 2, 3, 4 ),
+		'468x60' 	=> array( 3, 4, 5 ),
+		'488x160' 	=> array( 1, 2, 3, 4 ),
+		'620x203' 	=> array( 1, 2, 3, 4 ),
+		'760x80' 	=> array( 1, 2, 3, 4 )
+	);
+	$images = array();
+	foreach ( $sizes as $size => $variation ) {
+		foreach ( $variation as $value ) {
+			$images[ $size ][ "0" . $value ] = "/" . $size . "/bh-" . $size . "-0" . $value . "-dy.png";
+		}
+	}
 	return $images;
 }
 
@@ -434,12 +303,12 @@ class BHA_Widget extends WP_Widget {
 		if( isset( $instance[ 'bha-img-size' ] ) ) {
 			$bha_size = $instance[ 'bha-img-size' ];
 		} else {
-			$bha_size = '100x100';
+			$bha_size = '120x120';
 		}
 		if( isset( $instance[ 'bha-img-variation' ] ) ) {
 			$bha_variation = $instance[ 'bha-img-variation' ];
 		} else {
-			$bha_variation = '01';
+			$bha_variation = '03';
 		}
 		if( isset( $instance[ 'bha-img-align' ] ) ) {
 			$bha_align = $instance[ 'bha-img-align' ];
@@ -459,7 +328,7 @@ class BHA_Widget extends WP_Widget {
 		}
 		$bha_image_sizes = bha_image_sizes();
 		?>
-		<div class="bha-wid-preview"><img src="http://img.bluehost.com<?php echo $bha_image_sizes[ $bha_size ][ $bha_variation ]; ?>" /><br/><?php echo $bha_message; ?></div>
+		<div class="bha-wid-preview"><img src="<?php echo bha_get_image_link( $bha_size, $bha_variation ); ?>" /><br/><?php echo $bha_message; ?></div>
 		<p class="clear">
 		<label for="<?php echo $this->get_field_name( 'bha-img-title' ); ?>"><?php _e( 'Title:' ); ?></label> 
 		<input class="widefat" id="<?php echo $this->get_field_id( 'bha-img-title' ); ?>" name="<?php echo $this->get_field_name( 'bha-img-title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" />
@@ -519,7 +388,7 @@ class BHA_Widget extends WP_Widget {
 			$align = "";
 		}
 		$bha_img_links = bha_image_sizes();
-		echo "<a href='" . bha_get_link( $id ) . "' target='_blank'><img " . $align . "src='http://img.bluehost.com" . $bha_img_links[$size][$variation] . "' style='max-width:100%;'/></a>";
+		echo "<a href='" . bha_get_link( $id ) . "' target='_blank'><img " . $align . "src='" . bha_get_image_link( $size, $variation ) . "' style='max-width:100%;'/></a>";
 		echo $args['after_widget'];
 	}
 }
@@ -528,3 +397,22 @@ function register_bha_widget() {
 	register_widget( 'BHA_Widget' );
 }
 add_action( 'widgets_init', 'register_bha_widget' );
+
+function bha_load_updater() {
+	if ( is_admin() ) {
+		/*
+		Check class_exist because this could be loaded in a different plugin
+		*/
+		if( ! class_exists( 'GitHub_Updater' ) ) { 
+			require_once( plugin_dir_path( __FILE__ ) . 'updater/class-github-updater.php' );
+		}
+		if( ! class_exists( 'GitHub_Updater_GitHub_API' ) ) {
+			require_once( plugin_dir_path( __FILE__ ) . 'updater/class-github-api.php' );
+		}
+		if( ! class_exists( 'GitHub_Plugin_Updater' ) ) {
+			require_once( plugin_dir_path( __FILE__ ) . 'updater/class-plugin-updater.php' );
+		}
+		new GitHub_Plugin_Updater;
+	}
+}
+add_action( 'admin_init', 'bha_load_updater' );
